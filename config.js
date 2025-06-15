aea44a90824c08ab7ad1399a127d22101b5e3c30
chanugg <githubchanu@gmail.com>
2025-06-15 06:36:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Manu-MD&KM1zEKjK#wC7IcWbbeOwnXDW12j6kfs6T3VmOR834e0zriWKOBOw', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&TBc1BDTL#0hBTbcu3f90y4UVynEug3yASasjfIya5_GOZWMo5Fjs', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
 MONGODB: process.env.MONGODB || 'mongodb+srv://yesiy77559:edT6bMboc8n0jXDQ@manucustomize.n0rxcdd.mongodb.net/'
 };
